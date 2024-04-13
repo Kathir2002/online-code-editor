81fd4c17d41bc175ae577b0987e6b3b5b69cfeda
kathir <kathir@centiizen.com>
2024-04-13 16:01:46 +0530

    updated backend uri

--- server/src/passport/googlePassport.ts
+++ server/src/passport/googlePassport.ts
@@ -17,7 +17,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
       callbackURL:
-        "https://online-code-editor-l5r7.onrender.com/api/auth/google/callback",
+        "https://online-code-editor.cyclic.app/api/auth/google/callback",
       passReqToCallback: true,
     },
     async function (
