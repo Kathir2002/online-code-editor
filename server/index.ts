8b239d67e4ceb4d38aff35ceabb6fc48699edec6
kathir <kathir@centiizen.com>
2024-04-15 14:33:57 +0530

    intial stage

--- server/index.ts
+++ server/index.ts
@@ -24,15 +24,7 @@ app.use(
   })
 );
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET!,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: true,
-      sameSite: "none",
-    },
-  })
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
 );
 
 // app.use(
