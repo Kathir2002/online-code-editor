9c799417ed9d9041d14af45c2848c57b553bae0d
kathir <kathir@centiizen.com>
2024-04-15 18:55:32 +0530

    final deployment 2

--- server/src/controllers/authControllers.ts
+++ server/src/controllers/authControllers.ts
@@ -53,6 +53,14 @@ class auth {
       if (!existingUser) {
         return res.status(400).json({ message: "User not found" });
       }
+      if (existingUser && !password) {
+        return res
+          ?.status(400)
+          .json({
+            message:
+              'Please login to your account with "Login With Google" option',
+          });
+      }
 
       const passwordMatched = await bcrypt.compare(
         password,
