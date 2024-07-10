24cad57a2e1a50d4d7b56aea19925f487dffb366
kathir <kathir@centiizen.com>
2024-07-10 16:50:47 +0530

    vercel deployment

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const apiUrlDB = "https://online-code-editor-sable.vercel.app";
+export const apiUrlDB = "https://online-code-editor-seven.vercel.app";
 
 export const handleLoginWithGoogle = () => {
   window.open(`${apiUrlDB}/api/auth/google`, "_self");
